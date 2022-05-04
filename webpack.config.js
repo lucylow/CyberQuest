96f54431465d0706dce77732054ba611a7779a95
盧露 <low.lucyy@gmail.com>
2022-05-03 22:00:57 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,11 @@
+let config = {
+    debug: false,
+    spriteRoot:"/public/sprites",
+    hres:1920,
+    vres:1080,
+};
+
+module.exports = window.gameConfig?{...config, ...window.gameConfig}:config;
 
 const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
