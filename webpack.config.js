b23db3ed837abb7a72eff7d52a68b58409836633
盧露 <low.lucyy@gmail.com>
2022-05-03 21:57:36 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,14 @@
 
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+  entry: './src/engine.js',
+  output: {
+    filename: 'engine.js',
+    path: path.resolve(__dirname, 'public', 'js'),
+  },
+  optimization: {
+    minimizer: [new TerserPlugin({ extractComments: false })],
+  },
+};
